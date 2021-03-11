41066ffefa03e68d46e933d8321a268be5ae8d52
https://github.com/bu-AprilW/fsw-110.git <april.william@bryanuniversity.edu>
2021-03-11 00:30:34 -0600

    pokemon

--- assignments/week3/pokemon.js
+++ assignments/week3/pokemon.js
@@ -6,17 +6,19 @@ xhr.send()
 xhr.onreadystatechange = function() {
     if(xhr.readyState === 4 && xhr.status === 200) {
         const JSONdata = xhr.responseText
-        const data = JSON.parse(JSONdata);
-        const object = data.objects[0]
-        const x = object.pokemon
-        pokemon(x)
+        const data = JSON.parse(JSONdata)
+        showInfo(data.objects[0])
+        
     }
-}
+};
 
-function pokemon (arr) {
-    arr.map(function(pokemon) {
-        let h1 = document.createElement("h1")
-        h1.innerHTML = pokemon.name
-        document.body.append(h1)
-    })
-}
+function showInfo(arr) {
+    for (let i = 0; i < arr.pokemon.length; i++) {
+        const h1 = document.createElement("h1")
+        h1.textContent = arr.pokemon[i].name
+        document.body.appendChild(h1)
+        const h3 = document.createElement("h3")
+        h3.textContent = arr.pokemon[i].resource_uri
+        document.body.appendChild(h3)
+    }
+};
