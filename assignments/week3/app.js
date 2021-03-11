448211d76f88dad0baf006292c1eed55f2ee3981
https://github.com/bu-AprilW/fsw-110.git <april.william@bryanuniversity.edu>
2021-03-11 00:11:55 -0600

    pokemon

---
+++ assignments/week3/app.js
@@ -0,0 +1,67 @@
+const pokemonData = {
+    "meta": {
+        "limit": 1,
+        "next": null,
+        "offset": 0,
+        "previous": null,
+        "total-count": 1
+    },
+    "objects": [
+        {
+            "created": "2013-11-09T15:14:48.957604",
+            "modified": "2013-11-09T15:14:48.957565",
+            "name": "national",
+            "pokemon": [
+                {
+                    "name": "rattata",
+                    "resource_uri": "api/v1/pokemon/19/"
+                },
+                {
+                    "name": "charmander",
+                    "resource_uri": "api/v1/pokemon/4/"
+                },
+                {
+                    "name": "charmeleon",
+                    "resource_uri": "api/v1/pokemon/5/"
+                },
+                {
+                    "name": "wartortle",
+                    "resource_uri": "api/v1/pokemon/8/"
+                },
+                {
+                    "name": "blastoise",
+                    "resource_uri": "api/v1/pokemon/9/"
+                },
+                {
+                    "name": "caterpie",
+                    "resource_uri": "api/v1/pokemon/10/"
+                },
+                {
+                    "name": "metapod",
+                    "resource_uri": "api/v1/pokemon/11/"
+                },
+                {
+                    "name": "butterfree",
+                    "resource_uri": "api/v1/pokemon/12/"
+                },
+                {
+                    "name": "spearow",
+                    "resource_uri": "api/v1/pokemon/21/"
+                },
+                {
+                    "name": "kakuna",
+                    "resource_uri": "api/v1/pokemon/14/"
+                },
+                {
+                    "name": "beedrill",
+                    "resource_uri": "api/v1/pokemon/15/"
+                },
+                {
+                    "name": "pidgey",
+                    "resource_uri": "api/v1/pokemon/16/"
+                } 
+            ],
+            "resource_uri": "/api/v1/pokedex/1/"
+        }
+    ]
+}
